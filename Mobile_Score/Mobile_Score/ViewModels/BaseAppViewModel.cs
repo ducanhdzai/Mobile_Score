f5a73ae79fb3db69765a3948b55b4a3e6867bf92
Anh Nguyễn Văn Đức <anhnvd.job@gmail.com>
2023-08-20 11:59:55 +0700

    property changed event

---
+++ Mobile_Score/Mobile_Score/ViewModels/BaseAppViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Mobile_Score.ViewModels
+{
+    public class BaseAppViewModel : IDisposable, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+        protected void SetProperty<TValue>(ref TValue storeValue, TValue showValue, [CallerMemberName] string propertyName = null)
+        {
+            if (!object.Equals(storeValue, showValue))
+            {
+                storeValue = showValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+        }
+
+    }
+}
