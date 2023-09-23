f1b725461de60fac6517cacd15bc30d64b2318ad
Chu Duy <chuduyvybg@gmail.com>
2023-09-23 15:12:12 +0700

    Home page

--- Mobile_Score/Mobile_Score/App.xaml.cs
+++ Mobile_Score/Mobile_Score/App.xaml.cs
@@ -13,7 +13,7 @@ namespace Mobile_Score
             Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MjU4NjA5OEAzMjMyMmUzMDJlMzBpQ0pWUzl2SElsMzlCNkFicUFRMzhURGxhL3dDM3VGSnh2M3JMQkc3RnVvPQ==");
             InitializeComponent();
             DependencyService.Register<MockDataStore>();
-            MainPage = new LoginPage();
+            MainPage = new HomePage();
         }
 
         protected override void OnStart()
