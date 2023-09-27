355bf491ac849cc1c0bc38c50a5ffe3fb3ec61c1
Anh Nguyễn Văn Đức <anhnvd.job@gmail.com>
2023-09-28 00:08:08 +0700

    Thêm font icon, thêm menu bar

---
+++ Mobile_Score/Mobile_Score/Resource/Fonts/FluentIcons.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace Mobile_Score.Resource.Fonts
+{
+    [Preserve(AllMembers = true)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FluentIcons
+	{
+		public FluentIcons ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
